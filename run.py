dac83603916c514cd36920ea86da2047f0d4f650
Blandaren123 <toskha_91@hotmail.se>
2021-12-01 20:04:32 +0000

    add input for name tag and welcoming text

--- run.py
+++ run.py
@@ -3,6 +3,16 @@ import random
 """
 def welcome():
     """Welcome function"""
-    
- 
+    name = input(""">Welcome to Hangman Game! Enter your game name: <""").capitalize()
+
+    if name.isalpha() == true:
+        print(""">> Hi""",name,"""<<<
+        The computer will randomly choose a word and you will try to guess the word.
+        Good luck""")
+
+    else:
+        print('please enter letter only for name')
+        name = input('Enter game name here: ')
+        print('Hi!',name,)
+
 
