5fc1ad8bb2cdbe7eb4043fa93e51c206a1e03999
Blandaren123 <toskha_91@hotmail.se>
2021-12-02 19:38:32 +0000

    made randomness selector for the someWords list

--- run.py
+++ run.py
@@ -5,3 +5,13 @@ from collections import Counter
 """ Counters store elements as dictionary keys, and their counts are stored as dictionary values
 """
 
+someWords = '''audi jeep cadillac chevrolet chrysler dodge ford kia
+lexus nissan porche subaru skoda volvo''' 
+""" add som car brands to the list """
+
+someWords = someWords.split('')
+word = random.choice(someWords)
+""" let randomness choose word from the someWords list"""
+
+if __name__
+
