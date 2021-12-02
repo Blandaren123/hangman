346fc08ad5d4e57e7ef32a460f0ea94d4547b27b
Blandaren123 <toskha_91@hotmail.se>
2021-12-02 19:23:39 +0000

    add comments on random and collections and import counter

--- run.py
+++ run.py
@@ -1,19 +1,7 @@
 import random
-""" import random module
+""" import random module generator
 """
-def welcome():
-    """Welcome function"""
-    name = input(""">Welcome to Hangman Game! Enter your game name: <""").capitalize()
-    
-    if name.isalpha() == true:
-        """Only alphapets in name"""
-        print(""">> Hi""",name,"""<<<
-        The computer will randomly choose a word and you will try to guess the word.
-        Good luck""")
-
-    else:
-        print('please enter letter only for name')
-        name = input('Enter game name here: ')
-        print('Hi!',name,)
-
+from collections import Counter
+""" Counters store elements as dictionary keys, and their counts are stored as dictionary values
+"""
 
